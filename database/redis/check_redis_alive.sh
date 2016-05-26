f1eaa4e9849a48c6ceb5d7e9482f6a181a62805a
Vivi Tam <aiwife@gmail.com>
2016-05-26 14:10:36 +0800

    add redis conf

---
+++ database/redis/check_redis_alive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+MASTER_IP=$6
+LOCAL_IP='192.168.1.105'
+VIP='192.168.1.99'
+NETMASK='24'
+INTERFACE='bond0'
+if [ ${MASTER_IP} = ${LOCAL_IP} ]; then
+         /sbin/ip addr add ${VIP}/${NETMASK} dev ${INTERFACE}
+         /sbin/arping -q -c 3 -A ${VIP} -I ${INTERFACE}
+        exit 0
+else
+         /sbin/ip addr del ${VIP}/${NETMASK} dev ${INTERFACE}
+        exit 0
+fi
+exit 1
