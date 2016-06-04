e739869757b1a05a9a2b6c055a099935027c7db1
Vivi Tam <aiwife@gmail.com>
2016-06-04 15:49:00 +0800

    modirf cpfile

--- shell/cpfile.sh
+++ shell/cpfile.sh
@@ -6,17 +6,17 @@
 #date:2016/06/01 13:35                 ##
 #########################################
 
-SOU_DIR=/Users/vivitam/Downloads/doc_sample
-DES_DIR=/Users/vivitam/Downloads/doc_sample_pp
+SOU_DIR="/Volumes/Seagate Expansion Drive"
+DES_DIR="/Volumes/Seagate Expansion Drive/video"
 #dir size use mb,only number
 DIR_MAX_SIZE=300000
 
 function cpfile()
 {
-    mkdir -p $DES_DIR/300_$1;
+    mkdir -p "$DES_DIR"/300_$1;
     for f in $(cat filelist); do
-        mv "$f" $DES_DIR/300_$1;
-        DIR_SIZE=$(du -sm $DES_DIR/300_$1 | awk '{print $1}');
+        mv "$f" "$DES_DIR"/300_$1;
+        DIR_SIZE=$(du -sm "$DES_DIR"/300_$1 | awk '{print $1}');
         if [ "$DIR_SIZE" -le 300000 ] && [ "$DIR_SIZE" -ge 290000 ];then
            break
         fi
@@ -24,7 +24,7 @@ function cpfile()
 }
 
 for ((i=1;i<=15;i++));do
-    cd $SOU_DIR
-    find ./ -type f -name "*.ts" > filelist
+    cd "$SOU_DIR"
+    find ./ -path "./video" -prune -o -type f -name "*.ts" -print > filelist
     cpfile $i;
 done
