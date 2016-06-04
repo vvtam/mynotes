f91ac425f6995b27e80f089b4ca2973a28631f5d
Vivi Tam <aiwife@gmail.com>
2016-06-04 20:18:39 +0800

    add

--- shell/cpfile.sh
+++ shell/cpfile.sh
@@ -6,25 +6,31 @@
 #date:2016/06/01 13:35                 ##
 #########################################
 
-SOU_DIR="/Volumes/Seagate Expansion Drive"
-DES_DIR="/Volumes/Seagate Expansion Drive/video"
+SOU_DIR="/Users/vivitam/Documents/test space"
+DES_DIR="/Users/vivitam/Documents/test space/video"
 #dir size use mb,only number
 DIR_MAX_SIZE=300000
 
 function cpfile()
 {
-    mkdir -p "$DES_DIR"/300_$1;
-    for f in $(cat filelist); do
-        mv "$f" "$DES_DIR"/300_$1;
-        DIR_SIZE=$(du -sm "$DES_DIR"/300_$1 | awk '{print $1}');
-        if [ "$DIR_SIZE" -le 300000 ] && [ "$DIR_SIZE" -ge 290000 ];then
-           break
-        fi
-    done
+    if [ ! -d "$DES_DIR"/300_$1 ];then
+        mkdir -p "$DES_DIR"/300_$1
+    fi
+    DIR_SIZE=$(du -sm "$DES_DIR"/300_$1 | awk '{print $1}')
+    while [ "$DIR_SIZE" -le 28 ];do
+        read LINE
+        for f in "$LINE";do
+            cp "$f" "$DES_DIR"/300_$1/
+            DIR_SIZE_NEW=$(du -sm "$DES_DIR"/300_$1 | awk '{print $1}')
+            if [ "$DIR_SIZE_NEW" -le 300 ] && [ "$DIR_SIZE" -ge 250 ];then
+                break 
+            fi
+        done
+    done < filelist
 }
 
 for ((i=1;i<=15;i++));do
     cd "$SOU_DIR"
-    find ./ -path "./video" -prune -o -type f -name "*.ts" -print > filelist
-    cpfile $i;
+    find . -path "./video" -prune -o -type f -name "*.pdf" -print > filelist
+    cpfile $i
 done
