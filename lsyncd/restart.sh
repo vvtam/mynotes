734117244682f06134e9a050edbe6d1b961cb662
Vivi Tam <aiwife@gmail.com>
2019-04-23 15:14:11 +0800

    add first

---
+++ lsyncd/restart.sh
@@ -0,0 +1,2 @@
+ssh -p port root@ip "su -s /bin/bash - www -c '/usr/local/php/bin/php /data/wwwroot/cms/Webroot/launch/http.php restart'"
+su -s /bin/bash - www -c '/usr/local/php/bin/php /data/wwwroot/cms/Webroot/launch/http.php restart'
