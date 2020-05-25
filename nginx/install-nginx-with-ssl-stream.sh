ca74841d8a7f00241aa6eff60986eac7e039c73f
vvtam <aiwife@gmail.com>
2020-05-25 10:35:27 +0800

    update

---
+++ nginx/install-nginx-with-ssl-stream.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+workdir=$(pwd)
+installdir=/web/soft
+nginxversion=1.16.0
+
+yum install -y pcre pcre-devel openssl-devel openssl zlib zlib-devel
+
+useradd -M -s /sbin/nologin nginx
+
+tar -zxvf $workdir/nginx-"$nginxversion".tar.gz -C $workdir
+cd $workdir/nginx-$nginxversion
+./configure --prefix="$installdir"/nginx  --user=nginx  --group=nginx  --with-http_stub_status_module --with-http_ssl_module --with-pcre --with-stream && make -j$(nproc) && make install
+rm -rf $workdir/nginx-$nginxversion
